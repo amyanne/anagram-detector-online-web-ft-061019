e721284cdcbddecc213dfcade9acf0ba050b22d8
Amy Westcott <amy.westcott@yahoo.com>
2019-06-21 20:59:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :name, :word
+  
+  def intitialize(word, name)
+    self.name = name
+    @word = word 
+    
+  end 
+  
+  def self.match
+    anagram_array = []
+    name.split("")
+    name.sort 
+    @word.each do |letters|
+      letters.split("")
+      letters.sort 
+      if letters == name 
+        letters.join("")
+        anagram_array << letters
+      end 
+    end 
+    anagram_array
+    binding.pry
+ end 
+end 
