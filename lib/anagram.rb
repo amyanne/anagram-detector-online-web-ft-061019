f971db4e484a67180ece53245a05bf66d4fc6d53
Amy Westcott <amy.westcott@yahoo.com>
2019-06-21 21:06:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,6 @@ class Anagram
     name_array.sort
     word.each do |letters|
       letter_array = letters.split("")
-      binding.pry
       letter_array.sort 
       if letter_array == name_array 
         anagram_array << letters
